d8914c47c1c92a2b3f4f0f07d9a69eb037c97073
Kuba Kowalczykowski <kkow@MacBook-Pro-2.local>
2019-04-11 12:38:48 +0200

    Front changes

--- lib/mailbase.ex
+++ lib/mailbase.ex
@@ -6,4 +6,6 @@ defmodule Mailbase do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  #<!--<span style="display: inline"><%= @user.login_mail %></span>-->
 end
